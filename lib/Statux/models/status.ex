a47313b51090d27f8ab130ba5fcb417f00039c44
Lars Heinrichs <lars.heinrichs@web.de>
2022-02-13 14:37:52 +0100

    Feat: Add begin and end times to history

--- lib/Statux/models/status.ex
+++ lib/Statux/models/status.ex
@@ -16,9 +16,8 @@ defmodule Statux.Models.Status do
     field :transitioned_at, DateTime.t()
   end
 
-  # TODO: Add start/end time to history, e.g. {:low, %DateTime{}, %DateTime{} | nil}
-  def transition(nil, option) do
-    transition(nil, option, DateTime.utc_now())
+  def transition(data, option) do
+    transition(data, option, DateTime.utc_now())
   end
 
   def transition(nil, option, datetime) do
@@ -32,14 +31,55 @@ defmodule Statux.Models.Status do
   def transition(%__MODULE__{current: current_option} = status, option, datetime) do
     case current_option == option do
       true -> status
-      false ->
-        %{
-          status |
-            current: option,
-            transitioned_at: datetime,
-            transition_count: status.transition_count + 1,
-            history: [{option, status.transitioned_at, datetime} | status.history]
-        }
+      false -> update_status(status, option, datetime)
     end
+  end
+
+  # no history, has never transitioned before.
+  defp update_status(%__MODULE__{current: nil, history: []} = status, option, datetime) do
+    %{
+      status |
+        current: option,
+        transitioned_at: datetime,
+        transition_count: 1,
+    }
+  end
+
+  # no history, but has transitioned before
+  defp update_status(
+    %__MODULE__{
+      current: transition_from,
+      history: [],
+      transitioned_at: transitioned_at
+    } = status,
+    option,
+    datetime
+  ) do
+    %{
+      status |
+        current: option,
+        transitioned_at: datetime,
+        transition_count: status.transition_count + 1,
+        history: [{transition_from, transitioned_at, nil}]
+    }
+  end
+
+  # Has previously transitioned and a history -> update previous entry to add an end DateTime
+  defp update_status(
+    %__MODULE__{
+      current: transition_from,
+      history: [{previous_option, previous_started_at, _} | older_history],
+      transitioned_at: transitioned_at
+    } = status,
+    option,
+    datetime
+  ) do
+    %{
+      status |
+        current: option,
+        transitioned_at: datetime,
+        transition_count: status.transition_count + 1,
+        history: [{transition_from, transitioned_at, nil}, {previous_option, previous_started_at, transitioned_at} | older_history]
+    }
   end
 end
